9416daf96ddd4ab2279a9fad8b0e66b3b0e3631a
monika-octet <monika@octet.design>
2023-03-15 13:59:13 +0530

    learning path

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,13 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl);
 });
 // toast initialize
+$("select").multiselect();
+//success
+$("#success").click(function () {
+  Swal.fire({
+    icon: "success",
+    title: "Assignment Group 1 added to Learning Path.",
+    showConfirmButton: false,
+    timer: 1500,
+  });
+});
