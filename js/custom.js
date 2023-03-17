ac9a0d4d0da4894478e7eaddea53d5814120a274
monika-octet <monika@octet.design>
2023-03-17 20:29:09 +0530

    qc points

--- js/custom.js
+++ js/custom.js
@@ -16,75 +16,82 @@ $("#success").click(function () {
     timer: 1500,
   });
 });
-
+$("#success2").click(function () {
+  Swal.fire({
+    icon: "success",
+    title: "Assignment Group 1 added to Learning Path.",
+    showConfirmButton: false,
+    timer: 1500,
+  }).then(function () {
+    window.location = "assignment.html";
+  });
+});
 
 // create new group flow
 
-$('.add-assignment-content').hide();
-$('.step-2-footer').hide();
+$(".add-assignment-content").hide();
+$(".step-2-footer").hide();
 
-$('.step-1-save-btn').on('click', function () {
-  $('.step-1-content').hide();
-  $('.add-assignment-content').show();
-  $('.step-1-footer').hide();
-  $('.step-2-footer').show();
+$(".step-1-save-btn").on("click", function () {
+  $(".step-1-content").hide();
+  $(".add-assignment-content").show();
+  $(".step-1-footer").hide();
+  $(".step-2-footer").show();
 });
 
-$('#publish-check').on('click', function () {
-  if ($('#publish-check').is(':checked')) {
-    $('.step-2-publish-btn').removeClass('btn-secondary');
-    $('.step-2-publish-btn').addClass('btn-primary');
+$("#publish-check").on("click", function () {
+  if ($("#publish-check").is(":checked")) {
+    $(".step-2-publish-btn").removeClass("btn-secondary");
+    $(".step-2-publish-btn").addClass("btn-primary");
   }
 });
 
 // Assignment Tab Btn (Create New Link Change)
-$('#create-assignment').hide();
+$("#create-assignment").hide();
 
-$('#assignment-tab').on('click', function () {
-  $('#create-group').hide();
-  $('#create-assignment').show();
+$("#assignment-tab").on("click", function () {
+  $("#create-group").hide();
+  $("#create-assignment").show();
 });
 
-$('#groups-tab').on('click', function () {
-  $('#create-assignment').hide();
-  $('#create-group').show();
+$("#groups-tab").on("click", function () {
+  $("#create-assignment").hide();
+  $("#create-group").show();
 });
-
 
 // Add Task Table Row
-$('.added-task').hide();
-$('.added-task-auto-assign').hide();
-$('.weightage-without-error').hide();
-$('.add-new-task-link').hide();
-$('.first-task-row').hide();
-$('.weightage-row').hide();
+$(".added-task").hide();
+$(".added-task-auto-assign").hide();
+$(".weightage-without-error").hide();
+$(".add-new-task-link").hide();
+$(".first-task-row").hide();
+$(".weightage-row").hide();
 
-$('#assign-task-modal-btn').on('click', function () {
-  $('.first-task-row').show();
-  $('.weightage-row').show();
-  $('.add-new-task-modal').hide();
-  $('.add-new-task-link').show();
+$("#assign-task-modal-btn").on("click", function () {
+  $(".first-task-row").show();
+  $(".weightage-row").show();
+  $(".add-new-task-modal").hide();
+  $(".add-new-task-link").show();
 });
 
-$('.add-new-task-link').on('click', function () {
-  $('.added-task').show();
-  $('.switch-to-primary').removeClass('btn-outline-table');
-  $('.switch-to-primary').addClass('btn-outline-primary');
+$(".add-new-task-link").on("click", function () {
+  $(".added-task").show();
+  $(".switch-to-primary").removeClass("btn-outline-table");
+  $(".switch-to-primary").addClass("btn-outline-primary");
 });
 
-$('.assign-auto-btn').on('click', function () {
-  $('.added-task').hide();
-  $('.added-task-auto-assign').show();
-  $('.weightage-with-error').hide();
-  $('.weightage-without-error').show();
-  $('.weightage-error-text').removeClass('weightage-error-text');
-  $('.after-auto-publish-btn').removeClass('btn-secondary');
-  $('.after-auto-publish-btn').addClass('btn-primary');
+$(".assign-auto-btn").on("click", function () {
+  $(".added-task").hide();
+  $(".added-task-auto-assign").show();
+  $(".weightage-with-error").hide();
+  $(".weightage-without-error").show();
+  $(".weightage-error-text").removeClass("weightage-error-text");
+  $(".after-auto-publish-btn").removeClass("btn-secondary");
+  $(".after-auto-publish-btn").addClass("btn-primary");
 });
-
 
 // Select all checkboxes
 
 $("#checkAll").click(function () {
-  $('input:checkbox').not(this).prop('checked', this.checked);
+  $("input:checkbox").not(this).prop("checked", this.checked);
 });
